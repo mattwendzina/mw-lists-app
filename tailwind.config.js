2cc1b0d8944ecf17c88f67afb174fce97792b637
Matthew  Wendzina <matthewwendzina@Matthews-MacBook-Pro.local>
2022-04-30 14:11:07 +0100

    Adjust color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,18 @@ module.exports = {
     extend: {
       colors: {
         'oxford-blue': "#0E1428",
-        'oxford-blue-lighter': 'rgb(13 20 40 / 84%)',
         seashell: "#FFF3F0",
+        "honey-yellow": "#FDB321",
+        "myrtle-green": "#32746D",
+        'french-raspberry': "#C62F4F",
+        'oxford-blue-light': 'rgb(13 20 40 / 84%)',
+        "honey-yellow-light": 'hsl(40deg 98% 47% / 55%)',
+        "honey-yellow-lighter": 'hsl(40deg 98% 47% / 35%)',
         'blue-munsell': '#3891A6',
         'pacific-blue': '#47ABC2',
         'cinnamon-slate': "#CC5A71",
         flame: "#D95D39",
         tangerine: "#F18805",
-        marigold: "#f0a202",
       }
     }
   },

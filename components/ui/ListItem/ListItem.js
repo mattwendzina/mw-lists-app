6ba931495ccba8e5fc35e84c2cd933a9aa9cf36a
Matt Wendzina <matt.wendzina@bravissimo.com>
2022-05-08 10:53:12 +0100

    Reformating

--- components/ui/ListItem/ListItem.js
+++ components/ui/ListItem/ListItem.js
@@ -1,85 +1,79 @@
 import { FiDelete } from "react-icons/fi";
-import { MdCheckBoxOutlineBlank, MdCheckBox } from "react-icons/md"
+import { MdCheckBoxOutlineBlank, MdCheckBox } from "react-icons/md";
 import PropTypes from "prop-types";
 
-const ListItem = (
-    { item,
-        itemClasses,
-        itemBeingEdited,
-        deleteItemClasses,
-        setItemBeingEdited,
-        editItemHandler,
-        removeItem,
-        updateList,
-        checkItemClasses,
-        checkItem
-    }
-) => {
-    return (
-        <li className={itemClasses}
-            onClick={() => setItemBeingEdited({ id: item.id, name: item.name })}
+const ListItem = ({
+  item,
+  itemClasses,
+  itemBeingEdited,
+  deleteItemClasses,
+  setItemBeingEdited,
+  editItemHandler,
+  removeItem,
+  updateList,
+  checkItemClasses,
+  checkItem,
+}) => {
+  return (
+    <li
+      className={itemClasses}
+      onClick={() => setItemBeingEdited({ id: item.id, name: item.name })}
+    >
+      <button className={checkItemClasses} onClick={() => checkItem(item.id)}>
+        <MdCheckBoxOutlineBlank />
+      </button>
+      {itemBeingEdited.id === item.id ? (
+        <form
+          className="inline"
+          onSubmit={(e) => {
+            e.preventDefault();
+            setItemBeingEdited({});
+            updateList();
+          }}
         >
-            <button
-                className={checkItemClasses}
-                onClick={() => checkItem(item.id)}
-            >
-                <MdCheckBoxOutlineBlank />
-            </button>
-            {itemBeingEdited.id === item.id ?
-                <form
-                    className='inline'
-                    onSubmit={(e) => {
-                        e.preventDefault()
-                        setItemBeingEdited({})
-                        updateList()
-                    }}>
-                    <input
-                        className='bg-transparent text-center focus:outline-none px-2 py-1'
-                        onChange={(e) => editItemHandler(e, item)}
-                        onBlur={() => {
-                            setItemBeingEdited({})
-                            updateList()
-                        }
-                        }
-                        value={itemBeingEdited.name}
-                        autoFocus
-                    />
-                </form>
-                :
-                <button>
-                    <p
-                        className='bg-transparent text-center focus:outline-none px-2 py-1'
-                        value={item.name}
-                    >
-                        {item.name}
-                    </p>
-                </button>
-            }
-            <button
-                className={deleteItemClasses}
-                onClick={() => removeItem(item.id)}
-            >
-                <FiDelete />
-            </button>
-        </li>
-    )
-}
+          <input
+            className="bg-transparent text-center focus:outline-none px-2 py-1"
+            onChange={(e) => editItemHandler(e, item)}
+            onBlur={() => {
+              setItemBeingEdited({});
+              updateList();
+            }}
+            value={itemBeingEdited.name}
+            autoFocus
+          />
+        </form>
+      ) : (
+        <button>
+          <p
+            className="bg-transparent text-center focus:outline-none px-2 py-1"
+            value={item.name}
+          >
+            {item.name}
+          </p>
+        </button>
+      )}
+      <button className={deleteItemClasses} onClick={() => removeItem(item.id)}>
+        <FiDelete />
+      </button>
+    </li>
+  );
+};
 
 ListItem.propTypes = {
-    item: PropTypes.shape({
-        id: PropTypes.string,
-        name: PropTypes.string
-    }),
-    itemClasses: PropTypes.string,
-    itemBeingEdited: PropTypes.shape({
-        id: PropTypes.string,
-        name: PropTypes.string
-    }),
-    deleteItemClasses: PropTypes.string,
-    setItemBeingEdited: PropTypes.func,
-    editItemHandler: PropTypes.func,
-    removeItem: PropTypes.func,
-    checkItem: PropTypes.func,
-}
+  item: PropTypes.shape({
+    id: PropTypes.string,
+    name: PropTypes.string,
+  }),
+  itemClasses: PropTypes.string,
+  itemBeingEdited: PropTypes.shape({
+    id: PropTypes.string,
+    name: PropTypes.string,
+  }),
+  deleteItemClasses: PropTypes.string,
+  setItemBeingEdited: PropTypes.func,
+  editItemHandler: PropTypes.func,
+  removeItem: PropTypes.func,
+  checkItem: PropTypes.func,
+};
 
-export default ListItem
+export default ListItem;

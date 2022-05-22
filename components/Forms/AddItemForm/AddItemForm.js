02d73504b0d2b76b0baa18e849449795061bb44e
Matt Wendzina <matt.wendzina@bravissimo.com>
2022-05-22 16:17:52 +0100

    Put items in a div and allow passing classes optionally

--- components/Forms/AddItemForm/AddItemForm.js
+++ components/Forms/AddItemForm/AddItemForm.js
@@ -3,15 +3,17 @@ import PropTypes from "prop-types";
 import Input from "../../ui/Input/Input";
 import Button from "../../ui/Button/Button";
 
-const AddItemForm = ({ addItem, newItemHandler, item }) => (
+const AddItemForm = ({ addItem, newItemHandler, item, classes }) => (
   <form onSubmit={addItem} className="basis-1/3">
-    <Input label="Add new item" onChange={newItemHandler} value={item} />
-    <Button
-      name="Submit"
-      classes="mx-auto"
-      primary={item ? true : false}
-      disabled={!item ? true : false}
-    />
+    <div className={classes}>
+      <Input label="Add new item" onChange={newItemHandler} value={item} />
+      <Button
+        name="Submit"
+        classes="mx-auto"
+        primary={item ? true : false}
+        disabled={!item ? true : false}
+      />
+    </div>
   </form>
 );
 

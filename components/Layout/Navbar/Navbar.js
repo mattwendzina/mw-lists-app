01d94947af2759ec58cd2e79dde6fd7060a51954
Matt Wendzina <matt.wendzina@bravissimo.com>
2022-05-22 22:09:46 +0100

    Tidy up styling on navbar

--- components/Layout/Navbar/Navbar.js
+++ components/Layout/Navbar/Navbar.js
@@ -5,52 +5,57 @@ import { useSession, signOut } from "next-auth/react";
 const Navbar = ({ selectedListTitle }) => {
   const { data: session, status } = useSession();
 
-  const liClasses =
-    "py-0 px-4 transition ease duration-200 hover:text-honey-yellow";
+  const liClasses = "py-0 px-2 transition ease duration-200 flex-1 text-center";
 
   return (
-    <ul className="flex bg-oxford-blue text-white justify-between fixed top-0 left-0 w-full">
-      <Link href="/">
-        <a className="m-0 basis-1/4 text-left p-5">
-          <li className={liClasses}>Home</li>
-        </a>
-      </Link>
-      <h2 className="text-xl p-5 leading-none flex items-center">
-        {selectedListTitle && selectedListTitle}
-      </h2>
-      {status === "authenticated" ? (
-        <div className="flex basis-1/4 justify-end p-5">
-          <Link href="/lists">
-            <a>
-              <li className={liClasses}>Lists</li>
-            </a>
-          </Link>
-          <Link href="/createList">
-            <a>
-              <li className={liClasses}>Create</li>
-            </a>
-          </Link>
-          <Link href="/profile">
-            <a>
-              <li className={liClasses}>Profile</li>
-            </a>
-          </Link>
-          <Link href="/login">
-            <a
-              onClick={() => {
-                signOut({ redirect: false });
-              }}
-            >
-              <li className={liClasses}>Signout</li>
-            </a>
-          </Link>
-        </div>
-      ) : (
-        <Link href="/login">
-          <a className="basis-1/4 text-right">
-            <li className={liClasses}>Login</li>
+    <ul className="flex p-5 bg-oxford-blue text-white justify-between fixed top-0 left-0 w-full items-center">
+      <li className={`${liClasses} !text-left`}>
+        <Link href="/">
+          <a className="m-0 basis-1/4 text-left p-5 hover:text-honey-yellow">
+            Home
           </a>
         </Link>
+      </li>
+      <li className="text-xl leading-none flex items-center">
+        {selectedListTitle && selectedListTitle}
+      </li>
+      {status === "authenticated" ? (
+        <div className="flex basis-1/4 justify-end">
+          <li className={liClasses}>
+            <Link href="/lists">
+              <a>Lists</a>
+            </Link>
+          </li>
+          <li className={liClasses}>
+            <Link href="/createList">
+              <a>Create</a>
+            </Link>
+          </li>
+          <li className={liClasses}>
+            <Link href="/profile">
+              <a>Profile</a>
+            </Link>
+          </li>
+          <li className={liClasses}>
+            <Link href="/login">
+              <a
+                onClick={() => {
+                  signOut({ redirect: false });
+                }}
+              >
+                Signout
+              </a>
+            </Link>
+          </li>
+        </div>
+      ) : (
+        <li className={`${liClasses} text-right`}>
+          <Link href="/login">
+            <a className="basis-1/4 p-5 text-right hover:text-honey-yellow">
+              Login
+            </a>
+          </Link>
+        </li>
       )}
     </ul>
   );

847a911070ae334816cce2b1d908f33a05e1e99f
Matt Wendzina <matt.wendzina@bravissimo.com>
2022-05-22 16:18:53 +0100

    Put classes back into files as it sometimes breaks with tailwind

--- components/Forms/NewListForm/NewListForm.js
+++ components/Forms/NewListForm/NewListForm.js
@@ -1,13 +1,6 @@
 import { useEffect, useState, useRef } from "react";
 import { v4 as uuidv4 } from "uuid";
 
-import {
-  listContainerClasses,
-  editingItemClasses,
-  itemClasses,
-  deleteItemClasses,
-  titleInputClasses,
-} from "../../../helpers/classes";
 import { SuccessMessage } from "../../ui/Messages/messages";
 import TitleItem from "../../ui/TitleItem/TitleItem";
 import SubmitListButton from "../../ui/SubmitListButton/SubmitListButton";
@@ -15,6 +8,22 @@ import ListItem from "../../ui/ListItem/ListItem";
 import AddItemForm from "../AddItemForm/AddItemForm";
 
 import { createErrorMessage, setMessageDuration } from "../../../helpers/utils";
+
+const listContainerClasses = `basis-2/3 p-2 m-1 border border-honey-yellow rounded flex flex-col items-center`;
+const itemClasses = `
+  mx-auto relative w-60 hover:cursor-pointer group text-center
+  before:transition-all before:duration-500 
+  before:content-[" "] before:absolute 
+  before:border-b before:left-28 before:right-28 before:top-full before:border-honey-yellow 
+  hover:before:left-24 hover:before:right-24
+  `;
+const editingItemClasses = `mx-auto relative w-60 hover:cursor-pointer group text-center
+  before:transition-all before:duration-500 
+  before:content-[" "] before:absolute 
+  before:border-b before:left-24 before:right-24 before:top-full before:border-french-raspberry-light
+  `;
+const deleteItemClasses = `absolute left-full bottom-2/4 translate-y-2/4 opacity-0 transition ease duration-200 hover:cursor-pointer hover:text-french-raspberry group-hover:opacity-100 focus:opacity-100 focus:text-french-raspberry`;
+const titleInputClasses = `bg-transparent p-1 text-center text-xl focus:outline-none border-b border-honey-yellow placeholder:text-md placeholder:text-oxford-blue`;
 
 const NewListForm = () => {
   const titleRef = useRef(null);
--- components/Lists/SelectedList.js
+++ components/Lists/SelectedList.js
@@ -31,6 +31,23 @@ const SelectedList = ({ selectedList }) => {
     sendToDatabase();
   }, [listItems]);
 
+  const listClasses = `p-2 m-1 flex flex-col items-center`;
+  const itemClasses = `
+  mx-auto relative w-60 hover:cursor-pointer group text-center
+  before:transition-all before:duration-500 
+  before:content-[" "] before:absolute 
+  before:border-b before:left-28 before:right-28 before:top-full before:border-honey-yellow 
+  hover:before:left-24 hover:before:right-24
+  `;
+  const editingItemClasses = `mx-auto relative w-60 hover:cursor-pointer group text-center
+  before:transition-all before:duration-500 
+  before:content-[" "] before:absolute 
+  before:border-b before:left-24 before:right-24 before:top-full before:border-french-raspberry-light
+  `;
+  const deleteItemClasses = `absolute left-full bottom-2/4 translate-y-2/4 opacity-0 transition ease duration-200 hover:cursor-pointer hover:text-french-raspberry group-hover:opacity-100 focus:opacity-100 focus:text-french-raspberry`;
+  const checkItemClasses = `absolute right-full bottom-2/4 translate-y-2/4 opacity-0 transition ease duration-200 hover:cursor-pointer hover:text-french-raspberry group-hover:opacity-100 focus:opacity-100 focus:text-french-raspberry`;
+  const itemTextClasses = `bg-transparent text-center focus:outline-none px-2 py-1`;
+
   const editItemHandler = (e) => {
     setItemBeingEdited({ ...itemBeingEdited, name: e.target.value });
   };

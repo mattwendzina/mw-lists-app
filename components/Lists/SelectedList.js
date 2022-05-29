0f2583218d4fae780dea70603c6260290d20f726
Matthew Wendzina <matthewwendzina@matthews-mbp.home>
2022-05-29 08:29:54 +0100

    Import classes

--- components/Lists/SelectedList.js
+++ components/Lists/SelectedList.js
@@ -1,26 +1,15 @@
 import { useEffect, useState, useContext } from "react";
 
+import SetListsContext from "../../store/set-lists.context";
 import ListItem from "../ui/ListItem/ListItem";
 import AddItemForm from "../Forms/AddItemForm/AddItemForm";
-import { updateListInDb } from "../../lib/utils";
-import SetListsContext from "../../store/set-lists.context";
 
-const listClasses = `p-2 m-1 flex flex-col items-center`;
-const itemClasses = `
-  mx-auto relative w-60 hover:cursor-pointer group text-center
-  before:transition-all before:duration-500 
-  before:content-[" "] before:absolute 
-  before:border-b before:left-28 before:right-28 before:top-full before:border-honey-yellow 
-  hover:before:left-24 hover:before:right-24
-  `;
-const editingItemClasses = `mx-auto relative w-60 hover:cursor-pointer group text-center
-  before:transition-all before:duration-500 
-  before:content-[" "] before:absolute 
-  before:border-b before:left-24 before:right-24 before:top-full before:border-french-raspberry-light
-  `;
-const deleteItemClasses = `absolute left-full bottom-2/4 translate-y-2/4 opacity-0 transition ease duration-200 hover:cursor-pointer hover:text-french-raspberry group-hover:opacity-100 focus:opacity-100 focus:text-french-raspberry`;
-const checkItemClasses = `absolute right-full bottom-2/4 translate-y-2/4 opacity-0 transition ease duration-200 hover:cursor-pointer hover:text-french-raspberry group-hover:opacity-100 focus:opacity-100 focus:text-french-raspberry`;
-const itemTextClasses = `bg-transparent text-center focus:outline-none px-2 py-1`;
+import { updateListInDb } from "../../lib/utils";
+import {
+  listClasses, itemClasses, editingItemClasses,
+  deleteItemClasses, checkItemClasses, itemTextClasses
+} from "../../helpers/classes"
+
 
 const SelectedList = ({ selectedList }) => {
   const [itemInput, setItemInput] = useState("");
@@ -83,7 +72,7 @@ const SelectedList = ({ selectedList }) => {
               classes={setClasses(item)}
               itemBeingEdited={itemBeingEdited}
               setItemBeingEdited={(id, name) =>
-                setItemBeingEdited({ id: id, name: name })
+                setItemBeingEdited({ id, name })
               }
               editItemHandler={editItemHandler}
               checkItem={setListsCtx.checkItem}
